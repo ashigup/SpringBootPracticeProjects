b1ba69339fd515f0cea267cff3c4e887cb726466
Ashish Kumar <ashigup11@gmail.com>
2023-11-08 23:28:16 +0530

    all

--- SB-from-scratch/src/main/java/MyApplication.java
+++ SB-from-scratch/src/main/java/MyApplication.java
@@ -1,2 +1,15 @@
-package PACKAGE_NAME;public class MyApplication {
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+@SpringBootApplication
+public class MyApplication {
+//    @RequestMapping("/")
+//    String home() {
+//        return "Hello World!";
+//    }
+    public static void main(String[] args) {
+        System.out.println("hello");
+        SpringApplication.run(MyApplication.class, args);
+    }
 }
